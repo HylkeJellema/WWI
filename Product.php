4cd8cdeb199fd0c94e0626eaf5f74ee926212284
William <wilslooten@gmail.com>
2019-11-14 17:07:54 +0100

    ProductPagina

---
+++ Product.php
@@ -0,0 +1,81 @@
+<html>
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<nav class="navbar navbar-dark bg-light justify-content-between">
+    <a class="navbar-brand"><img src="imgs/logo.png" alt="logo"></a>
+    <form class="form-inline">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+</nav>
+<body>
+<?php
+include "productfuncties.php";
+$con = MaakVerbinding();
+$product = ProductOphalen($con);
+?>
+
+<div class="container">
+    <div class="card">
+        <div class="row">
+            <aside class="col-sm-5 border-right">
+                <article class="gallery-wrap">
+                    <div class="img-big-wrap">
+                        <div> <a href="#"><img src="imgs/usb_missile_launcher.jpg"></a></div>
+                    </div> <!-- slider-product.// -->
+                </article> <!-- gallery-wrap .end// -->
+            </aside>
+            <aside class="col-sm-7">
+                <article class="card-body p-5">
+                    <h3 class="title mb-3"><?php echo $product['naam']; ?></h3>
+
+                    <p class="price-detail-wrap">
+	<span class="price h3 text-warning">
+		<span class="currency">EU €</span><span class="num"><?php echo $product['price'] ?></span>
+	</span>
+
+                    </p>
+                    <dl class="item-property">
+                        <dt>Description</dt>
+                        <dd><p>Hier komt een mooie beschrijving </p></dd>
+                    </dl>
+
+                    <dl class="param param-feature">
+                        <dt>Delivery</dt>
+                        <dd>Nederland, Europe</dd>
+                    </dl>
+
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-5">
+                            <dl class="param param-inline">
+                                <dt>Quantity: </dt>
+                                <dd>
+                                    <select class="form-control form-control-sm" style="width:70px;">
+                                        <option> 1 </option>
+                                        <option> 2 </option>
+                                        <option> 3 </option>
+                                    </select>
+                                </dd>
+                            </dl>  <!-- item-property .// -->
+                        </div> <!-- col.// -->
+                        <div class="col-sm-7">
+
+                        </div>
+                    </div>
+                    <hr>
+                    <a href="#" class="btn btn-lg btn-primary text-uppercase"> Buy now </a>
+                    <a href="#" class="btn btn-lg btn-outline-primary text-uppercase"> <i class="fas fa-shopping-cart"></i> Add to cart </a>
+                </article> <!-- card-body.// -->
+            </aside> <!-- col.// -->
+        </div> <!-- row.// -->
+    </div> <!-- card.// -->
+
+
+</div>
+
+
+
+</body>
+</html>
---
+++ productfuncties.php
@@ -0,0 +1,27 @@
+<?php
+function MaakVerbinding()
+{
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $databasename = 'wideworldimporters';
+    $connection = mysqli_connect($host, $user, $pass, $databasename);
+    if (!$connection) die("Unable to connect to MySQL: " . mysqli_error($connection));
+    return $connection;
+}
+
+function ProductOphalen($connection)
+{
+    $id = $_GET["id"];
+    if (isset($id)) {
+        $statement = mysqli_prepare($connection, "SELECT StockItemID, StockItemName, UnitPrice FROM stockitems WHERE StockItemID = ?");
+        mysqli_stmt_bind_param($statement, 'i', $id);
+        mysqli_stmt_execute($statement);
+        mysqli_stmt_bind_result($statement, $StockItemId, $StockItemName, $price);
+        mysqli_stmt_fetch($statement);
+        $result = array("nummer" => $StockItemId, "naam" => $StockItemName, "price" => $price);
+        mysqli_stmt_close($statement);
+        return $result;
+    }
+}
+?>
