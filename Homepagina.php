62733a98687fe5511cbcd56fae0c2249fc660eb2
youriWindesheim <S1145122@student.windesheim.nl>
2019-11-15 12:00:56 +0100

    Homepagina opzetje toegevoegd

---
+++ Homepagina.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+    <title>World Wide Imports</title>
+</head>
+<body>
+<link rel="stylesheet" type="text/css" href="Style.css">
+<form method="get" action="Index.php">
+    <a href="Index.php">
+        <img class="logo" src="imgs/wide-world-importers-logo-small.png" height="80">
+    </a>
+    <!--
+            <input type="text" name="Search">
+            <input type="submit" name="Zoeken" value="Zoeken">
+
+            <a href="Login.php">
+                Login
+            </a>
+    -->
+    <a href="Index.php">
+        <img class="winkelmand" src="imgs/winkelmandje.png" height="50" width="50">
+    </a>
+    <br>
+
+</form>
+
+<link rel="stylesheet" type="text/css" href="dropdownStyles.css">
+<nav>
+    <ul>
+        <li><a class="Home" href="Index.php">Home</a></li>
+        <li><a href="#">Lijst1</a>
+            <ul>
+                <li><a href="#">prod1</a></li>
+                <li><a href="#">prod2</a></li>
+                <li><a href="#">prod3</a></li>
+                <li><a href="#">prod4</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Lijst2</a>
+            <ul>
+                <li><a href="#">prod1</a></li>
+                <li><a href="#">prod2</a></li>
+                <li><a href="#">prod3</a></li>
+                <li><a href="#">prod4</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Lijst3</a>
+            <ul>
+                <li><a href="#">prod1</a></li>
+                <li><a href="#">prod2</a></li>
+                <li><a href="#">prod3</a></li>
+                <li><a href="#">prod4</a></li>
+            </ul></li>
+        <li><a class="Login" href="Login.php">Login</a></li>
+    </ul>
+
+    <br>
+
+    <a href="Index.php">
+        <img class = kortingsbanner src="imgs/kortingsbanner.jpg" width="1200" height="200px">
+    </a>
+
+</body>
+</html>
