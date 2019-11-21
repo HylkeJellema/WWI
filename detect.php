412555659c3a173c9c71221907d9dee5b54306ae
Hylke Jellema <hylke.jellema@hotmail.com>
2019-11-21 09:28:56 +0100

    update

--- detect.php
+++ detect.php
@@ -5,9 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
+<nav class="navbar navbar-dark bg-light justify-content-between">
+    <a class="navbar-brand"><img src="imgs/logo.png" alt="logo"></a>
+    <form class="form-inline" action="Lijstpagina.php">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+</nav>
 <body>
-<div>Teachable Machine Image Model</div>
+<div>Zoeken naar product</div>
 <button type="button" onclick="init()">Start</button>
 <div id="webcam-container"></div>
 <div id="label-container"></div>
