016a2f4935e74e8e960526be682189feaae5a37e
youriWindesheim <S1145122@student.windesheim.nl>
2019-11-29 15:39:18 +0100

    werkende dynamische categorie

--- NAVTest.php
+++ NAVTest.php
@@ -2,40 +2,49 @@
 
 <html>
 <link rel="stylesheet" type="text/css" href="Style.css">
-    <link rel="stylesheet" type="text/css" href="dropdownStyles.css">
+
     <link rel="stylesheet" type="text/css" href="NAVTest.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 </html>
 
-     <!-- NAVBAR -->
-
-
-        <!--
-                <a href="Login.php">
-                    Login
-                </a>
-        -->
-
-
-        <!-- <a href="detect.php">
-                <img class="winkelmand" src="imgs/cam.png" height="75" width="60">
-        </a> -->
-
-
-
-        <link rel="stylesheet" type="text/css" href="dropdownStyles.css">
+<link rel="stylesheet" type="text/css" href="dropdownStyles.css">
         <nav>
             <ul>
-                <li><a class="Home" href="Homepagina.php">Home</a></li>
+
                 <?php
-                include 'categorieTest.php';
+
+                    function categorieLijst(){
+
+                    include "productfuncties.php";
+
+
+                    $host = "localhost";
+                    $databasename = "wideworldimporters";
+                    $user = "root";
+                    $pass = ""; //eigen password invullen
+                    $port = 3306;
+                    $connection = mysqli_connect($host, $user, $pass, $databasename, $port);
+
+
+                    $sql = "SELECT StockGroupName FROM stockgroups";
+                    $result = mysqli_query($connection, $sql)
+                    or die("Error: " . mysqli_error($connection));
+
+                    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                        $naam = $row["StockGroupName"];
+                        //print($naam . "<br>");
+                        print("<li><a href ='#'>" . $naam . "</a></li>");
+                    }
+
+
+                    SluitVerbinding($connection);
+
+                }
+
                 categorieLijst();
 ?>
-                <li><a class="Login" href="Login.php">Inloggen</a></li>
 
-
-                <li>
             </ul>
 
 
