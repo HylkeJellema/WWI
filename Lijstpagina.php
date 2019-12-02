f56eef925ce98cddfd070155ba99f7649f3517cc
Bavirion <fakereclame@hotmail.com>
2019-12-02 11:03:44 +0100

    Als database foto ontbreekt, ImageComingSoon.png wordt gebruikt

--- Lijstpagina.php
+++ Lijstpagina.php
@@ -60,7 +60,13 @@ while($row = mysqli_fetch_array($zoekresultaten)){
     ?>
     <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4 col-xl-3 text-center" style="padding-bottom: 15px;">
         <div class="card border-primary" style="width: 19rem; margin: 0 auto; height: 430px;">
-            <?php    echo '<img src="data:image/jpg;base64,' .  base64_encode($row['Photo'])  . '" />';     ?>
+            <?php
+            if($row['Photo'] != "") {
+                echo '<img class="card-img-top" src="data:image/jpg;base64,' . base64_encode($row['Photo']) . '" alt="Card image cap"/>';
+            } else {
+                echo '<img class="card-img-top" src="imgs/ImageComingSoon.png" alt="Card image cap">';
+            }
+            ?>
             <div class="card-body">
                 <h5 class="card-title">
                     <?php
