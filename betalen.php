4fbd76fbc7f46bd3e49307d82cae5ebfa3a7b255
Hylke Jellema <hylke.jellema@hotmail.com>
2019-12-06 13:39:27 +0100

    begin van het betaal systeem

---
+++ betalen.php
@@ -0,0 +1,18 @@
+<?php
+include 'NAVTest.php';
+waza();
+include "lijstpaginafuncties.php";
+include_once "productfuncties.php";
+
+$mollie = new \Mollie\Api\MollieApiClient();
+$mollie->setApiKey("test_B9wWs47JaxUVjJVCj9ukyF8bFvvxED");
+
+$payment = $mollie->payments->create([
+    "amount" => [
+        "currency" => "EUR",
+        "value" => "10.00"
+    ],
+    "description" => "My first API payment",
+    "redirectUrl" => "https://webshop.example.org/order/12345/",
+    "webhookUrl"  => "https://webshop.example.org/mollie-webhook/",
+]);
