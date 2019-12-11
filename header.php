cf9a1bb8db3368e245920b9580461cad77485def
Bavirion <fakereclame@hotmail.com>
2019-12-11 13:17:23 +0100

    Zoeken fix

--- header.php
+++ header.php
@@ -17,7 +17,7 @@ include ("init.php");
     </a>
     <div class="collapse navbar-collapse" id="navbarSupportedContent" style="margin-top: 10px">
         <form class="form-inline my-2 my-lg-0" action="Lijstpagina.php?">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
 
