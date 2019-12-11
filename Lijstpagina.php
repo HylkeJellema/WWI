3b048666692044fd7a99472b6dc51817e0813674
Xander <hartman.xander@gmail.com>
2019-12-11 11:04:55 +0100

    Grootte

--- Lijstpagina.php
+++ Lijstpagina.php
@@ -118,7 +118,7 @@ waza();
                         }
                         while($row = mysqli_fetch_array($zoekresultaten)){
                             ?>
-                            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-4 col-xl-3 text-center" style="padding-bottom: 15px;">
+                            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-4 col-xl-3 text-center" style="padding-bottom: 15px; margin-right: 40px; margin-left: 40px;">
                                 <div class="card" style="width: 19rem; margin: 0 auto; height: 430px; border-color: #00AEEF;">
                                     <?php
                                     if($row['Photo'] != "") {
