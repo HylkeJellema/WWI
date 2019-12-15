561c0b4411d79d1476d133717b51a6e126512e93
Hylke <hylke.jellema@hotmail.com>
2019-12-15 20:58:26 +0100

    opruimen

--- verwerk.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (isset($_POST["user"] && $_POST == "hylke")){
-    if (isset($_POST["pass"] && $_POST == "123")){
-        $_SESSION["ingelogd"]=true;
-        $_SESSION["user"] = $_POST["user"];
-        $_SESSION["pass"] = $_POST["pass"];
-    } else {print ("Verkeerde gegevens")}
-} else {print ("Verkeerde gegevens")}
